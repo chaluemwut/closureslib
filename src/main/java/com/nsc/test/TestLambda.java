496cffa63e80812151bf6a7d21c11c017f22f716
chaluemwut@hotmail.com <Rv]]jkojkiyd123>
2013-08-28 14:49:19 +0700

    Init for maven project

---
+++ src/main/java/com/nsc/test/TestLambda.java
@@ -0,0 +1,15 @@
+package com.nsc.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: linux
+ * Date: 8/28/13
+ * Time: 2:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestLambda {
+
+    public static void main(String [] args){
+        System.out.println("hello world");
+    }
+}
